d6ae0ad0d6c8a0ad2faf4a3b706b153caa9bb75a
arshia <arshiashahrokhi822@gmail.com>
2024-01-16 15:05:40 +0330

    fix the bug of clinic class

--- OOP.py
+++ OOP.py
@@ -1,11 +1,17 @@
 import sqlite3 as sql
 import requests
+import json
+from datetime import datetime, timedelta
 
-data = requests.get('http://127.0.0.1:5000/slots')
+
+#data = requests.get('http://127.0.0.1:5000/slots')
 
 conn = sql.connect(r'D:\AP\project\clinic\main.db')
 cur = conn.cursor()
-
+cur.execute('DROP table if exists clinics')
+conn.commit()
+cur.execute('DROP table if exists users')
+conn.commit()
 # Create table if it doesn't exist already
 cur.execute('''CREATE TABLE IF NOT EXISTS users(
             id INTEGER PRIMARY KEY, 
@@ -15,6 +21,53 @@ cur.execute('''CREATE TABLE IF NOT EXISTS users(
             role TEXT,
             status INTEGER);
         ''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS clinics(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            address TEXT,
+            phone_number TEXT,
+            services TEXT,
+            availability INTEGER);''' )
+
+cur.execute('''CREATE TABLE IF NOT EXISTS queue(
+            appointment_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+            status VARCHAR(20) NOT NULL, 
+            datetime DATETIME NOT NULL, 
+            user_id INTEGER NOT NULL, 
+            clinic_id INTEGER NOT NULL, 
+            appointment_cost REAL NOT NULL,
+            FOREIGN KEY(user_id) REFERENCES user(user_id), 
+            FOREIGN KEY(clinic_id) REFERENCES clinics(id))''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS notification(
+            notification_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+            user_id INTEGER NOT NULL, 
+            message TEXT NOT NULL, 
+            date_sent DATETIME NOT NULL, 
+            FOREIGN KEY(user_id) REFERENCES user(user_id))''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS payment(
+            payment_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            user_id INTEGER NOT NULL,
+            clinic_id INTEGER NOT NULL,
+            appointment_id INTEGER NOT NULL,
+            paid_amount REAL NOT NULL,
+            payment_date DATETIME NOT NULL,
+            payment_description TEXT,
+            FOREIGN KEY(user_id) REFERENCES user(user_id),
+            FOREIGN KEY(clinic_id) REFERENCES clinic(clinic_id),
+            FOREIGN KEY(appointment_id) REFERENCES queue(appointment_id))''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS review(
+            review_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            user_id INTEGER NOT NULL,
+            clinic_id INTEGER NOT NULL,
+            rating INTEGER NOT NULL,
+            comment TEXT,
+            date_of_review DATETIME NOT NULL,
+            FOREIGN KEY(user_id) REFERENCES user(user_id),
+            FOREIGN KEY(clinic_id) REFERENCES clinic(clinic_id))''')
 
 cur.execute('DELETE FROM users;')    
 users = [(10,'arshia','arshia@gmail.com','Salam0011','patient',0),(20,'parsa','parsa@gmail.com','strong','patient',0),(30,'arash','arash@','password','staff',0),(40,'mamad','@gmail.com','be to che','pashent',0),(50,'sadra','sadra@gmail.com','ramz','staff',0)]
@@ -47,9 +100,9 @@ class users:
                         name,
                         email,
                         password,
-                        role
+                        role,
                         status) 
-                        VALUES(?,?,?,?,?);''',(user_id,full_name,email,password,role,0))
+                        VALUES(?,?,?,?,?,?);''',(user_id,full_name,email,password,role,0))
             conn.commit()
             print("user created successfully")
             return True
@@ -98,4 +151,267 @@ class users:
                     =? order by date asc """,(users.get_info()["email"],))
         
 class clinic:
-    pass
+    def __init__(self,clinic_id,name,address,phone_number,services,availability):
+        self.clinic_id    = clinic_id
+        self.name         = name
+        self.address      = address
+        self.phone_num    = phone_number
+        self.services     = services
+        self.availability = availability
+    
+    def create_new_clinic(clinic_id,name,address,phone_number,services,availability):
+        #check if clinic exists or not
+        cur.execute('''SELECT phone_number FROM clinics;''')
+        conn.commit
+        phone_numbers = [phone_number[0] for phone_number in cur.fetchall()]
+        if phone_number in phone_numbers:
+            print("clinic already exists")
+            return False
+
+        else:
+            cur.execute('''INSERT INTO clinics(
+                        id,
+                        name,
+                        address,
+                        phone_number,
+                        services,
+                        availability) 
+                        VALUES(?,?,?,?,?,?);''',(clinic_id,name,address,phone_number,services,availability))
+            conn.commit()
+            print("clinic created successfully")
+            return True  
+          
+    def update(name,address,phone_number,services,availability):
+        pass
+
+#    def view_appointmant():
+#        """
+ #       This function is used to display all the appointments of a particular clinic.
+#        It takes one argument i.e., clinic_id and returns None.
+#        """
+#        def view_appointments_by_clinic(clinic_id):
+#            cur.execute('''SELECT a.*, u.full_name 
+ #                          FROM appointments AS a
+ #                          JOIN users AS u ON u.id = a.doctor_id 
+ #                          WHERE a.clinic_id = ? 
+#                           ORDER BY a.date ASC''', (clinic_id,))
+ #           appointments = cur.fetchall()
+  #          for appointment in appointments:
+#                print(appointment)
+ #               # If no arguments are passed then it will show all the appointments from all the clinic
+#        def view_appointments():
+ #           cur.execute('''SELECT a.*, u.full_name 
+ #                           FROM appointments AS a
+ #                           JOIN users AS u ON u.id = a.doctor_id 
+ #                           ORDER BY a.date ASC''')
+ #           appointments = cur.fetchall()
+ #           for appointment in appointments:
+#                print(appointment)
+                        
+    @classmethod
+    def fetch_slots_data(cls):
+        # Fetch data from the /slots endpoint of your app
+        app_url = 'http://127.0.0.1:5000'  # Update this with the actual URL of your app
+        response = requests.get(f'{app_url}/slots')
+        data = response.json()
+        
+        if response.status_code == 200:
+            slots_data = response.json()
+            return slots_data
+        else:
+            print(f"Failed to fetch slots data. Status Code: {response.status_code}")
+            return {}
+
+    @classmethod
+    def update_clinic_availability_from_api(cls):
+        slots_data = cls.fetch_slots_data()
+
+        for clinic_id, availability in slots_data.items():
+            cur.execute('''INSERT INTO clinics VALUES(?,?,?,?,?,?)''', (clinic_id,None,None,None,None, availability))
+            
+        conn.commit()
+        print("Clinic availability updated successfully from API.")
+##############################
+
+    @classmethod
+    def view_appointments(cls, clinic_id):
+        
+        cur.execute('''SELECT * FROM queue WHERE clinic_id = ? AND status = 'Booked' ORDER BY datetime''', (clinic_id,))
+        appointments = cur.fetchall()
+
+        if appointments:
+            print("Clinic's booked appointments:")
+            for appointment in appointments:
+                appointment_id, status, datetime_str, user_id, _, appointment_cost = appointment
+                print(f"Appointment ID: {appointment_id}, Status: {status}, DateTime: {datetime_str}, User ID: {user_id}, Cost: {appointment_cost}")
+        else:
+            print("The clinic doesn't have any booked appointments.")
+
+    @classmethod
+    def set_availability(cls, clinic_id, new_availability):
+        cur.execute('''UPDATE clinics SET availability = ? WHERE id = ?''', (new_availability, clinic_id))
+        conn.commit()
+        print("Availability updated successfully.")
+
+    @classmethod
+    def update_clinic_info(cls, clinic_id):
+        
+        cur.execute('''SELECT * FROM clinics WHERE id = ?''', (clinic_id,))
+        existing_clinic = cur.fetchone()
+
+        if existing_clinic:
+            print("1. Update Name\n2. Update Address\n3. Update Phone Number\n4. Update Services")
+            choice = input("Enter your choice (1/2/3/4): ")
+
+            if choice == '1':
+                new_name = input("Enter the new name: ")
+                cur.execute('''UPDATE clinics SET name = ? WHERE id = ?''', (new_name, clinic_id))
+            elif choice == '2':
+                new_address = input("Enter the new address: ")
+                cur.execute('''UPDATE clinics SET address = ? WHERE id = ?''', (new_address, clinic_id))
+            elif choice == '3':
+                new_phone_number = input("Enter the new phone number: ")
+                cur.execute('''UPDATE clinics SET phone_number = ? WHERE id = ?''', (new_phone_number, clinic_id))
+            elif choice == '4':
+                new_services = input("Enter the new services (comma-separated): ")
+                cur.execute('''UPDATE clinics SET services = ? WHERE id = ?''', (new_services, clinic_id))
+            else:
+                print("Invalid choice. clinics info update failed.")
+                return
+
+            conn.commit()
+            print("Clinic info update successful.")
+        else:
+            print("Clinic not found. Clinic info update failed.")
+
+class Notification:
+    def __init__(self, notification_id, user_id, message, date_sent):
+
+        self.notification_id = notification_id
+        self.user_id = user_id
+        self.message = message
+        self.date_sent = date_sent
+
+    @classmethod
+    def send_notification(cls, user_id, message):
+        date_sent = datetime.now()
+        cur.execute('''INSERT INTO notification (user_id, message, date_sent) 
+                              VALUES (?, ?, ?)''', (user_id, message, date_sent))
+        conn.commit()
+        print("Notification sent successfully.")
+
+class Payment:
+    def __init__(self, payment_id, user_id, clinic_id, appointment_id, paid_amount, payment_date, payment_description):
+        self.payment_id = payment_id
+        self.user_id = user_id
+        self.clinic_id = clinic_id
+        self.appointment_id = appointment_id
+        self.paid_amount = paid_amount
+        self.payment_date = payment_date
+        self.payment_description = payment_description
+
+    @classmethod
+    def process_payment(cls, user_id, clinic_id, payment_description):
+    
+        cur.execute('''SELECT appointment_id, status, appointment_cost FROM queue 
+                    WHERE user_id = ? AND clinic_id = ? AND status IN ('Booked', 'Rescheduled') 
+                    ORDER BY datetime DESC LIMIT 1''', (user_id, clinic_id))
+        appointment_details = cur.fetchone()
+
+        if appointment_details:
+            appointment_id, status, appointment_cost = appointment_details
+            paid_amount = appointment_cost if status == 'Booked' else 0.0
+            payment_date = datetime.now()
+            conn.execute('''INSERT INTO payment (user_id, clinic_id, appointment_id, paid_amount, payment_date, payment_description)
+                        VALUES (?, ?, ?, ?, ?, ?)''', (user_id, clinic_id, appointment_id, paid_amount, payment_date, payment_description))
+            conn.commit()
+            print("Payment processed successfully.")
+        else:
+            print("No eligible appointment found for payment.")
+
+class Queueing:
+    def __init__(self, status, datetime, user_id, clinic_id, appointment_id, appointment_cost):
+        self.status = status
+        self.datetime = datetime
+        self.user_id = user_id
+        self.clinic_id = clinic_id
+        self.appointment_id = appointment_id
+        self.appointment_cost = appointment_cost
+
+    @classmethod
+    def book_appointment(cls, user_id, clinic_id, appointment_cost):
+
+        current_datetime = datetime.now()
+        status = 'Booked'
+
+        cur.execute('''INSERT INTO queue (status, datetime, user_id, clinic_id, appointment_cost) 
+                    VALUES (?, ?, ?, ?, ?)''', (status, current_datetime, user_id, clinic_id, appointment_cost))
+        conn.commit()
+        print("Appointment booked successfully.")
+
+    @classmethod
+    def cancel_missed_appointments(cls, user_id):
+
+        status = 'Booked'
+        cur.execute('''UPDATE queue SET status = 'Cancelled' 
+                    WHERE status = ? AND user_id = ? AND datetime < ?''', (status, user_id, datetime.now()))
+        conn.commit()
+        print("Missed appointments cancelled successfully.")
+
+    @classmethod
+    def reschedule_appointment(cls, user_id, clinic_id, new_datetime):
+
+        status = 'Booked'
+        cur.execute('''UPDATE queue SET datetime = ? 
+                    WHERE status = ? AND user_id = ? AND clinic_id = ? AND datetime > ?''',
+                           (new_datetime, status, user_id, clinic_id, datetime.now()))
+        conn.commit()
+        print("Appointment rescheduled successfully.")
+
+class Review:
+    def __init__(self, review_id, user_id, clinic_id, rating, comment, date_of_review):
+        self.review_id = review_id
+        self.user_id = user_id
+        self.clinic_id = clinic_id
+        self.rating = rating
+        self.comment = comment
+        self.date_of_review = date_of_review
+
+    @classmethod
+    def submit_review(cls, user_id, clinic_id, rating, comment):
+
+        current_datetime = datetime.now()
+        cur.execute('''INSERT INTO review (user_id, clinic_id, rating, comment, date_of_review)
+                    VALUES (?, ?, ?, ?, ?)''', (user_id, clinic_id, rating, comment, current_datetime))
+        conn.commit()
+        print("Review submitted successfully.")
+
+    @classmethod
+    def update_review(cls, review_id, new_rating, new_comment):
+        cur.execute('''SELECT date_of_review FROM review WHERE review_id = ?''', (review_id,))
+        review_date = cur.fetchone()
+
+        if review_date and datetime.now() - datetime.strptime(review_date[0], "%Y-%m-%d %H:%M:%S") < timedelta(hours=3):
+            cur.execute('''UPDATE review SET rating = ?, comment = ? WHERE review_id = ?''',
+                            (new_rating, new_comment, review_id))
+            conn.commit()
+            print("Review updated successfully.")
+        else:
+            print("Review update not allowed after 3 hours of submission.")
+
+    @classmethod
+    def generate_average_ratings_table(cls):
+        cur.execute('''SELECT clinic_id, AVG(rating) AS average_rating
+                    FROM review
+                    GROUP BY clinic_id''')
+        result = cur.fetchall()
+            
+        if result:
+            print("Clinic ID | Average Rating")
+            print("----------------------------")
+            for row in result:
+                print(f"{row[0]}         | {row[1]:.2f}")
+        else:
+            print("No reviews available.")
+
+clinic.update_clinic_availability_from_api()
